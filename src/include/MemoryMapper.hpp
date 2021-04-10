8e279783486631296d19c519a176603aefe47506
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-04-09 20:30:35 -0700

    started writing PIA

--- src/include/MemoryMapper.hpp
+++ src/include/MemoryMapper.hpp
@@ -2,57 +2,64 @@
 #include <cstdint>
 #include <cstdio>
 
+//for pattern matching designated memory locations
+typedef struct memzone
+{
+	uint16_t start;
+	uint16_t length;
+	uint16_t *mirrors;	// array of mirror locations
+	uint8_t numMirrors; // how many locations it is mirrored at
+} memzone_t;
+
 class MemoryMapper // interface for generating a memory map for an entire system, base class simply creates 64k of ram
 {
 private:
-	uint8_t* addressSpace; // can't be directly modified
+	uint8_t *addressSpace; // can't be directly modified
 	uint64_t addrSpaceSize;
 
-    struct memZone{
-        memZoneName name;
-        uint16_t start;
-        uint16_t length;
-        uint16_t* mirrors; // array of mirror locations
-        uint8_t numMirrors; // how many locations it is mirrored at
-    };
 public:
+	memzone_t *memZones; // each child will have diff memzones
+
 	MemoryMapper()
 	{
 		this->addressSpace = new uint8_t[65536](); // children might not generate actual addressSpace in memory
 		this->addrSpaceSize = 65536;
 	};
-	
+
 	MemoryMapper(uint64_t addrSpaceSize)
 	{
 		this->addressSpace = new uint8_t[addrSpaceSize](); // children might not generate actual addressSpace in memory
 		this->addrSpaceSize = addrSpaceSize;
 	};
-	
+
 	virtual ~MemoryMapper()
 	{
 		delete[] this->addressSpace;
 	};
-	
-	virtual uint8_t read(uint16_t address) { return this->addressSpace[address];};
-	
-	virtual uint16_t read16(uint16_t address) {
+
+	virtual uint8_t read(uint16_t address) { return this->addressSpace[address]; };
+
+	virtual uint16_t read16(uint16_t address)
+	{
 		return ((this->addressSpace[address] << 8) | this->addressSpace[address + 1]);
 	};
-	
+
 	virtual bool write(uint16_t address, char byte)
 	{
-		if (address > this->addrSpaceSize) return false;
+		if (address > this->addrSpaceSize)
+			return false;
 		this->addressSpace[address] = byte;
 		return true;
 	};
 
-	virtual bool writeArray(uint16_t startAddress,  uint8_t bytes[], uint16_t programLength)
+	virtual bool writeArray(uint16_t startAddress, uint8_t bytes[], uint16_t programLength)
 	{
-		if (startAddress + programLength > addrSpaceSize) return false; // would cause memory leak
-		
-		for(int i = 0; i < programLength; i++) this->addressSpace[startAddress + i] = bytes[i];
-		
+		if (startAddress + programLength > addrSpaceSize)
+			return false; // would cause memory leak
+
+		for (int i = 0; i < programLength; i++)
+			this->addressSpace[startAddress + i] = bytes[i];
+
 		return true;
 	};
 };
-
--- src/include/PIA.hpp
+++ src/include/PIA.hpp
@@ -5,9 +5,37 @@
 // spec: https://www.problemkaputt.de/2k6specs.htm#memoryandiomap
 
 //Periferal Interface Adapter - in charge of I/O
-class PIA: public MemoryMapper 
+class PIA : public MemoryMapper
 {
 public:
-    
-    
+    /** //FIXME add PIA mirrors
+     * PIA RAM Mirrors (Step 100h,400h):
+     * PIA RAM is selected by A12=0, A9=0, A7=1. Located in memory at 0080h-00FFh, 0180h-01FFh, 0480h-04FFh, 0580h-05FFh, etc. 
+     * The mirror at 0180h is particulary important because it allows the CPU to access RAM for stack operations.
+     * PIA I/O Mirrors (Step 2h,8h,100h,400h):
+     * PIA I/O is selected by A12=0, A9=1, A7=1. 
+     * Located in memory at 0280h-02FFh, 0380h-03FFh, 0680h-06FFh, 0780h-07FFh, etc. 
+     * Each 80h-area contains 16 mirrors of the PIA I/O ports at XX80h, XX88h, XX90h, XX98h, etc. 
+     * And, each 8h-area contains two copies of INTIM (eg. 0284h,0286h) and INSTAT (eg. 0285h,0287h).
+     */
+    memzone_t memZones[11] =
+        {
+            {0x08, 0x7F, nullptr, 0}, //RAM     11111111  128 bytes RAM (in PIA chip) for variables and stack
+            {0x0280, 1, nullptr, 0},  //SWCHA   11111111  Port A; input or output  (read or write)
+            {0x0281, 1, nullptr, 0},  //SWACNT  11111111  Port A DDR, 0= input, 1=output
+            {0x0282, 1, nullptr, 0},  //SWCHB   11111111  Port B; console switches (read only)
+            {0x0283, 1, nullptr, 0},  //SWBCNT  11111111  Port B DDR (hardwired as input)
+            {0x0284, 1, nullptr, 0},  //INTIM   11111111  Timer output (read only)
+            {0x0280, 1, nullptr, 0},  //INSTAT  11......  Timer Status (read only, undocumented)
+            {0x0294, 1, nullptr, 0},  //TIM1T   11111111  set 1 clock interval (838 nsec/interval)
+            {0x0295, 1, nullptr, 0},  //TIM8T   11111111  set 8 5lock interval (6.7 usec/interval)
+            {0x0296, 1, nullptr, 0},  //TIM64T  11111111  set 64 clock interval (53.6 usec/interval)
+            {0x0297, 1, nullptr, 0},  //T1024T  11111111  set 1024 clock interval (858.2 usec/interval)
+        };
+
+    PIA() {}
+
+    uint8_t read(){
+        
+    }
 };
